47fc6e5d2251ada4cf58e36ec400d0e78657bd00
Luca Ingold <luca.ingold@gmail.com>
2024-02-02 23:12:53 +0100

    install script

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
-cd ~/e-ink-frame-client
-#cd ~/code/frameBackend
+#! /usr/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo -n 'db' | gnome-keyring-daemon --unlock
 source .venv/bin/activate
-python3 app.py
+python3 "${SCRIPT_DIR}/app.py"
 deactivate
