f0a9ca6e868ea3f38edf3067fd6b67ce0f72bb7a
Luca Ingold <luca.ingold@gmail.com>
2024-02-02 23:16:05 +0100

    install script

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo -n 'db' | gnome-keyring-daemon --unlock
+cd ${SCRIPT_DIR}
 source .venv/bin/activate
-python3 "${SCRIPT_DIR}/app.py"
+python3 app.py
 deactivate
