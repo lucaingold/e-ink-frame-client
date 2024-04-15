f89e06d34b7cbd98231305421f646d04585afeda
Luca Ingold <luca.ingold@gmail.com>
2024-04-15 16:12:54 +0200

    improvements

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo -n 'db' | gnome-keyring-daemon --unlock
 cd ${SCRIPT_DIR}
+git pull
 source .venv/bin/activate
 python3 app.py
 deactivate
