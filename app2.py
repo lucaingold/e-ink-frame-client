7a3b9ab7f77c660e71e953988e7976975557b425
Luca Ingold <luca.ingold@gmail.com>
2024-05-06 10:54:59 +0200

    improvements

--- app2.py
+++ app2.py
@@ -3,7 +3,7 @@ import time
 
 # Set the GPIO mode and pin number
 GPIO.setmode(GPIO.BOARD)
-switch_pin = 33  # Example GPIO pin (you can change it)
+switch_pin = 13  # Example GPIO pin (you can change it)
 
 # Setup the GPIO pin for input
 GPIO.setup(switch_pin, GPIO.IN)
@@ -21,6 +21,7 @@ try:
 
         # Add a small delay to prevent excessive CPU usage
         time.sleep(0.2)
+
 
 except KeyboardInterrupt:
     # Clean up GPIO on keyboard interrupt
