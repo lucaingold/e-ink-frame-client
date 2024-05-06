e563a77c2128fef253cff0bfaef888e1f712c239
Luca Ingold <luca.ingold@gmail.com>
2024-05-06 10:10:01 +0200

    improvements

---
+++ app2.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+import time
+
+# Set the GPIO mode and pin number
+GPIO.setmode(GPIO.BOARD)
+switch_pin = 11  # Example GPIO pin (you can change it)
+
+# Setup the GPIO pin for input
+GPIO.setup(switch_pin, GPIO.IN)
+
+try:
+    while True:
+        # Read the state of the switch
+        switch_state = GPIO.input(switch_pin)
+
+        # Determine the state of the switch
+        if switch_state == GPIO.HIGH:
+            print("Switch is ON")
+        else:
+            print("Switch is OFF")
+
+        # Add a small delay to prevent excessive CPU usage
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    # Clean up GPIO on keyboard interrupt
+    GPIO.cleanup()
