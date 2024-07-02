7ea30c33ba2ee8b813c1b9ea7c70b9f97b8f7ba2
Luca Ingold <luca.ingold@gmail.com>
2024-07-02 23:51:11 +0200

    init

--- src/e_ink_screen.py
+++ src/e_ink_screen.py
@@ -1,24 +1,18 @@
 import io
 import logging
 import os
-
-import threading
 import time
 from PIL import Image, ImageEnhance
 from omni_epd import displayfactory, EPDNotFoundError
 
 DISPLAY_TYPE = "waveshare_epd.it8951"
 
-
 class EInkScreen:
     def __init__(self, screen_width=1600, screen_height=1200, brightness_factor=1.0, darkness_threshold=0.5):
         self.config_dict = {}
         self.epd = None
-        self.image_base = None
-        self.image_display = None
         self.width = screen_width
         self.height = screen_height
-        # self.lock = threading.Lock()
         self.brightness_factor = brightness_factor
         self.darkness_threshold = darkness_threshold
 
@@ -37,17 +31,38 @@ class EInkScreen:
             logging.info("ctrl + c:")
             raise
         except BaseException as e:
-            logging.error(e)
+            logging.error(f"Error initializing e-ink screen: {e}")
             raise
 
     async def display_image(self, image_data):
         try:
-            # with self.lock:
-                # display_image = self.enhance_brightness(image_data) #TODO
             self.display_image_on_epd(image_data)
             time.sleep(5)
         except Exception as e:
-            logging.error("Error decoding and displaying the image:", str(e))
+            logging.error(f"Error decoding and displaying the image: {e}")
+
+    def display_image_on_epd(self, display_image):
+        logging.info("display_image_on_epd")
+        try:
+            image_file_path = "save/image.jpeg"
+            if os.path.exists(image_file_path):
+                os.remove(image_file_path)
+                logging.info("Existing file removed: %s", image_file_path)
+            display_image.save(image_file_path)
+            logging.info("Image saved to disk: %s", image_file_path)
+
+            image_display = self.enhance_brightness(display_image)
+
+            logging.info("Prepare e-ink screen")
+            self.epd.prepare()
+            logging.info("Clear e-ink screen")
+            self.epd.clear()
+            logging.info("Display image on e-ink screen")
+            self.epd.display(image_display)
+            logging.info("Send e-ink screen to sleep")
+            self.epd.sleep()
+        except Exception as e:
+            logging.error(f"Error displaying image on e-ink screen: {e}")
 
     def enhance_brightness(self, img):
         img_gray = img.convert('L')
@@ -70,27 +85,6 @@ class EInkScreen:
             return self.brightness_factor
         else:
             return self.brightness_factor - 0.2
-
-    def display_image_on_epd(self, display_image):
-        logging.info("display_image_on_epd")
-        image_file_path = "save/image.jpeg"
-        if os.path.exists(image_file_path):
-            os.remove(image_file_path)
-            logging.info("Existing file removed: %s", image_file_path)
-        display_image.save(image_file_path)
-        logging.info("Image saved to disk: %s", image_file_path)
-
-        self.image_display = display_image.copy()
-        logging.info("Prepare e-ink screen")
-        self.epd.prepare()
-        logging.info("Clear e-ink screen")
-        self.epd.clear()
-        logging.info("Display image on e-ink screen")
-        self.epd.display(self.image_display)
-        logging.info("Send e-ink screen to sleep")
-        self.epd.sleep()
-        self.epd.close()
-
 
     @staticmethod
     def set_rotate(width, height, rotate=0):
