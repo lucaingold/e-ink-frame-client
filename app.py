4e5481be81be52ed9bba2ee31220a1dcb286bbdb
Luca Ingold <luca.ingold@gmail.com>
2024-08-05 23:07:14 +0200

    init

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import logging
 import socket
 import json
 import time
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
 from contextlib import asynccontextmanager
 import asyncio
 from fastapi import FastAPI, Depends, HTTPException
@@ -60,6 +60,8 @@ print()
 # epd.width = screen_config.width
 # epd.height = screen_config.height
 image_rotate = 0
+
+
 # width, height = set_rotate(epd.width, epd.height, image_rotate)
 
 
@@ -116,23 +118,63 @@ def display_image_on_epd(display_image):
         # logging.info("Image saved to disk: %s", image_file_path)
 
         # image_display = self.enhance_brightness(display_image)
-
-        logging.info("Prepare e-ink screen")
-        logging.info("Clear e-ink screen")
-        display.clear()
-        logging.info("Display image on e-ink screen")
-        display.frame_buf.paste(0xFF, box=(0, 0, display.width, display.height))
-        # Image.open(display_image)
-        dims = (display.width, display.height)
-        display_image.thumbnail(dims)
-        paste_coords = [dims[i] - display_image.size[i] for i in (0,1)]  # align image with bottom of display
-        display.frame_buf.paste(display_image, paste_coords)
-        display.draw_full(constants.DisplayModes.GC16)
-        logging.info("Send e-ink screen to sleep")
+        partial_update(display)
+        # logging.info("Prepare e-ink screen")
+        # logging.info("Clear e-ink screen")
+        # display.clear()
+        # logging.info("Display image on e-ink screen")
+        # display.frame_buf.paste(0xFF, box=(0, 0, display.width, display.height))
+        # # Image.open(display_image)
+        # dims = (display.width, display.height)
+        # display_image.thumbnail(dims)
+        # paste_coords = [dims[i] - display_image.size[i] for i in (0, 1)]  # align image with bottom of display
+        # display.frame_buf.paste(display_image, paste_coords)
+        # display.draw_full(constants.DisplayModes.GC16)
+        # logging.info("Send e-ink screen to sleep")
         # epd.sleep()
         # display.epd.sleep()
     except Exception as e:
         logging.error(f"Error displaying image on e-ink screen: {e}")
+
+
+
+def partial_update(display):
+    print('Starting partial update...')
+
+    # clear image to white
+    display.frame_buf.paste(0xFF, box=(0, 0, display.width, display.height))
+
+    print('  writing full...')
+    _place_text(display.frame_buf, 'partial', x_offset=-display.width//4)
+    display.draw_full(constants.DisplayModes.GC16)
+
+    # TODO: should use 1bpp for partial text update
+    print('  writing partial...')
+    _place_text(display.frame_buf, 'update', x_offset=+display.width//4)
+    display.draw_partial(constants.DisplayModes.DU)
+
+
+def _place_text(img, text, x_offset=0, y_offset=0):
+    '''
+    Put some centered text at a location on the image.
+    '''
+    fontsize = 80
+
+    draw = ImageDraw.Draw(img)
+
+    try:
+        font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeSans.ttf', fontsize)
+    except OSError:
+        font = ImageFont.truetype('/usr/share/fonts/TTF/DejaVuSans.ttf', fontsize)
+
+    img_width, img_height = img.size
+    text_width = font.getlength(text)
+    text_height = fontsize
+
+    draw_x = (img_width - text_width) // 2 + x_offset
+    draw_y = (img_height - text_height) // 2 + y_offset
+
+    draw.text((draw_x, draw_y), text, font=font)
 
 
 def on_connect(client, userdata, flags, rc):
